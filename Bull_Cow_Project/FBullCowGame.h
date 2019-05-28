c45707aeb8ce9d96e0f677db75202ed02bfcaaf2
Justin Brown <justinmbrown2016@gmail.com>
2019-05-28 00:08:43 -0400

    Updated header

--- Bull_Cow_Project/FBullCowGame.h
+++ Bull_Cow_Project/FBullCowGame.h
@@ -4,6 +4,12 @@
 using FString = std::string;
 using int32 = int;
 
+// VALUES INT. TO ZERO
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
 
 class FBullCowGame
 {
@@ -14,12 +20,15 @@ public:
 	int32 GetCurrentTry() const;
 	bool IsGameWon() const;
 
-	bool CheckGuessValidity(FString); //TODO RETUNR BETTER VALUE
+	bool CheckGuessValidity(FString); //TODO RETURN BETTER VALUE
 	void Reset(); //TODO CHANGE FROM VOID
+
+	FBullCowCount SubmitGuess(FString);
 
 //PLEASE IGNORE ME
 private:
 	int32 MyCurrentTry = 1;
 	int32 MyMaxTries = 8;
 
+	FString MyHiddenWord = "planet";
 };
